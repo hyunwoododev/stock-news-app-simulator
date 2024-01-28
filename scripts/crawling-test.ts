7bc00f1662cdf2e89292e702de261d72d9de8d38
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-01-29 01:50:48 +0900

    feat: crawling test

--- scripts/crawling-test.ts
+++ scripts/crawling-test.ts
@@ -1,4 +1,46 @@
-async function main() {
-  console.log("start crawler");
+import puppeteer from "puppeteer";
+
+class Crawler {
+  browser: any = null;
+  page: any = null;
+  targetUrl = "https://coding-lks.tistory.com/category";
+  blogData: any = null;
+
+  async setConfig() {
+    this.browser = await puppeteer.launch({
+      headless: false,
+    });
+    this.page = await this.browser.newPage();
+    this.blogData = [];
+  }
+
+  async accessSite() {
+    await this.page.goto(this.targetUrl);
+  }
+
+  async getPartOfPost() {
+    await this.page.waitForSelector("#body > ul > li", {
+      timeout: 1000,
+    });
+
+    const posts = await this.page.$$("#body > ul > li");
+    for (const post of posts) {
+      const name = await post.$eval("a", (e: any) => e.innerText);
+      const date = await post.$eval(".date", (e: any) => e.innerText);
+      this.blogData.push({
+        name,
+        date,
+      });
+    }
+  }
 }
-main();
+
+async function crawl() {
+  const crawler = new Crawler();
+  await crawler.setConfig();
+  await crawler.accessSite();
+  await crawler.getPartOfPost();
+  console.log(crawler.blogData);
+}
+
+crawl();
